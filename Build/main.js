754bdbc8c57521586f5e9507351c08d900ce0f5a
etudiants <etudiants@FMED-2881-03-P.cifss.ulaval.ca>
2021-06-09 01:16:47 -0400

    Ajout de le page home

--- Build/main.js
+++ Build/main.js
@@ -1,2 +1,11 @@
-// à venir?
+//page d'acceuil
+let buttonAcceuil= document.getElementById("boutton_acceuil")
+let body= document.getElementById("body")
+let acceuil=document.getElementById("acceuil")
 
+    buttonAcceuil.onclick = function(){AcceuilDisparait()};
+    function AcceuilDisparait(){
+        acceuil.classList.add("acceuil2");
+        acceuil.classList.remove("acceuil1");
+        body.classList.remove("scrollbar_cachee");
+    }
